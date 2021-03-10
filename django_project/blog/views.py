77e09de93b021a714a934dc7ba3a4bcb7d5bfe31
Chandni Chandwani <chandni.chandwani@gatewaygroup.com>
2021-03-10 14:43:58 +0530

    done

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,10 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'Person1',
+        'title': 'Post1',
+        'content': 'First post',
+        'date_posted': 'January 1, 2021'
+    },
+    {
+        'author': 'Person2',
+        'title': 'Post2',
+        'content': 'Second post',
+        'date_posted': 'January 2, 2021'
+    }
+
+]
+
 
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title': 'About'})
